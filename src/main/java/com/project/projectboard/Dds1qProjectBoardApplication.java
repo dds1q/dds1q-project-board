3cb06aef5b0ab598685f5d8c022d3f4130b2273f
dds1q <dom955057@gmail.com>
2022-09-13 16:50:59 +0900

    #1-boot
    부트 설치

---
+++ src/main/java/com/project/projectboard/Dds1qProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dds1qProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dds1qProjectBoardApplication.class, args);
+    }
+
+}
