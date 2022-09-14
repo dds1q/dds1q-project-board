ffe91d606da9326472b52e5c0b9d47cdd5a2d263
dds1q <dom955057@gmail.com>
2022-09-14 12:20:00 +0900

    #11-rest

---
+++ src/test/java/com/project/projectboard/controller/DataRestTest.java
@@ -0,0 +1,66 @@
+package com.project.projectboard.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@DisplayName("Data Rest - API테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+    public DataRestTest (@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticles_thenReturnArticlesJsonResponse() throws Exception {
+
+        mvc.perform( get("/api/articles") )
+            .andExpect(status().isOk())
+            .andExpect( content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void givenNothing_whenRequestingArticle_thenReturnArticleJsonResponse() throws Exception {
+
+        mvc.perform( get("/api/articles/1") )
+            .andExpect( status().isOk() )
+            .andExpect( content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticleComments_thenReturnArticleCommentsJsonResponse() throws Exception {
+
+        mvc.perform( get("/api/articleComments") )
+            .andExpect( status().isOk() )
+            .andExpect( content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void givenNothing_whenRequestingArticleComment_thenReturnArticleCommentJsonResponse() throws Exception {
+
+        mvc.perform( get("/api/articleComments/1") )
+            .andExpect( status().isOk() )
+            .andExpect( content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+
+
+
+}
