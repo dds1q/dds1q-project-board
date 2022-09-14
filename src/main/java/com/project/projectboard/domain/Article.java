b45521403716f38934b12f37bf0d9448dddc880b
dds1q <dom955057@gmail.com>
2022-09-14 11:23:38 +0900

    #10 - AuditingField 추출

--- src/main/java/com/project/projectboard/domain/Article.java
+++ src/main/java/com/project/projectboard/domain/Article.java
@@ -23,9 +23,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
     @Index( columnList = "createdAt" ),
     @Index( columnList = "createdBy" )
 } )
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
@@ -43,20 +42,6 @@ public class Article {
     @OrderBy("id")
     @OneToMany( mappedBy = "article", cascade = CascadeType.ALL )
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    // meta data
-    @CreatedDate
-    @Column( nullable = false )
-    private LocalDateTime createdAt;
-
-    @CreatedBy @Column( nullable = false, length = 100 )
-    private String createdBy;
-
-    @LastModifiedDate @Column( nullable = false )
-    private LocalDateTime modifiedAt;
-
-    @LastModifiedBy @Column( nullable = false ,length = 100 )
-    private String modifiedBy;
 
     public Article(){}
 
---
+++ src/main/java/com/project/projectboard/domain/AuditingFields.java
@@ -0,0 +1,39 @@
+package com.project.projectboard.domain;
+
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditingFields {
+
+    @DateTimeFormat( iso = ISO.DATE_TIME )
+    @CreatedDate
+    @Column( nullable = false , updatable = false )
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column( nullable = false, length = 100 , updatable = false )
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column( nullable = false )
+    private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column( nullable = false ,length = 100 )
+    private String modifiedBy;
+
+}
